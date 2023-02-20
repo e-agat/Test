cb2f06d23916a6a1e1a95219f168c3e30f8ed1a1
Evgeniya <kapatoy@yandex.ru>
2023-02-20 22:30:31 +0300

    HW008

---
+++ HW008/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число: ");
+int valeu = Convert.ToInt32(Console.ReadLine());
+
+//Console.WriteLine("Квадрат числа " + valeu + " равен: " + valeu * valeu);
+
+Console.WriteLine("Квадрат числа " + valeu + " равен: " + Math.Pow(valeu, 2));
+
+
