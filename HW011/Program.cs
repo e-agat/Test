c7de02e8c99808e9b30977c5177ded16de186c87
Evgeniya <kapatoy@yandex.ru>
2023-02-21 13:06:47 +0300

    HW011

---
+++ HW011/Program.cs
@@ -0,0 +1,19 @@
+﻿//Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+//4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4" 
+//2 -> " -2, -1, 0, 1, 2"
+
+
+Console.Clear();
+
+Console.WriteLine("Введите число: ");
+
+int N = Convert.ToInt32(Console.ReadLine());
+int count = N;
+int value = -N;
+
+while (value < count)
+{
+    value++;
+    Console.Write(value);
+    Console.Write(", ");
+}
