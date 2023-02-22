73131ebd2c82e507663a16b524c4a5b7fdb0c6f3
Evgeniya <kapatoy@yandex.ru>
2023-02-22 22:28:29 +0300

    HW014

---
+++ HW014/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = { 1, 21, 13, 4, 8, 51, 6, 72, 8 };
+
+int n = array.Length;
+int find = 8;
+
+int index = 0;
+
+while(index<n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
