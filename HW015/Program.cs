1bdae0a86698ba9be1ab99c7557ef1c9e9e9c3ae
Evgeniya <kapatoy@yandex.ru>
2023-02-23 18:25:22 +0300

    HW015

--- HW015/Program.cs
+++ HW015/Program.cs
@@ -14,28 +14,32 @@ void PrintArray(int[] col)
 {
     int count = col.Length;
     int position = 0;
-    while(position<count)
+    while (position < count)
     {
         Console.WriteLine(col[position]);
         position++;
     }
 }
 
-int IndexOf(int[] collection, int find);
-int count = Collection.Length;
-int index = 0;
-int position = -1;
-
-while(index<count)
+int IndexOf(int[] collection, int find)
 {
-    if(Collection[index] == find)
-    {
-        position = index;
-        break;
-    }
-    index++;
-    return position;
 
+
+    int count =collection.Length;
+    int index = 0;
+    int position = -1;
+
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+
+    }
+    return position;
 }
 
 int[] array = new int[10];
@@ -44,5 +48,5 @@ FillArray(array);
 PrintArray(array);
 Console.WriteLine();
 
-int pos = IndexOf[array, 4];
+int pos = IndexOf(array, 44);
 Console.WriteLine(pos);
