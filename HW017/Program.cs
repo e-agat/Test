290503707293d03681eb54ccc361bdd77a32f0a6
Evgeniya <kapatoy@yandex.ru>
2023-02-24 00:10:33 +0300

    HW017

---
+++ HW017/Program.cs
@@ -0,0 +1,16 @@
+﻿//Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа.
+//456 -> 46
+
+Console.Clear();
+
+int num = new Random().Next(100, 1000);
+Console.WriteLine($"Число: {num}");
+
+int firstValue = num / 100;
+int thirdValue = num % 10;
+
+int result = firstValue * 10 + thirdValue;
+
+Console.WriteLine($"Результат удаления второй цифры из числа: {result}");
+
+
