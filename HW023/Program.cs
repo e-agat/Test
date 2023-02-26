7e15577cd4998287f1a6e26505f9ddbe45869b53
Evgeniya <kapatoy@yandex.ru>
2023-02-26 19:14:26 +0300

    HW023

---
+++ HW023/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.  
+// A (3,6); B (2,1) -> 5,09  A (7,-5); B (1,-1) -> 7,21
+
+Console.Clear();
+
+Console.WriteLine("Введите координаты точки A: ");
+Console.Write("X ");
+int aX = int.Parse(Console.ReadLine());
+Console.Write("Y ");
+int aY = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите координаты точки B: ");
+Console.Write("X ");
+int bX = int.Parse(Console.ReadLine());
+Console.Write("Y ");
+int bY = int.Parse(Console.ReadLine());
+
+
+double d = Math.Sqrt(Math.Pow((bX - aX), 2) + Math.Pow((bY - aY), 2));
+
+Console.WriteLine($"Расстояние между точками А и В равно {d}");
