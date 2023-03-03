4195323dccacf4877c683c82ac67a48a2b81c6d8
Evgeniya <kapatoy@yandex.ru>
2023-03-03 19:38:56 +0300

    HW026

---
+++ Seminar/HW026/Program.cs
@@ -0,0 +1,31 @@
+﻿/*Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+456 -> 3
+78 -> 2
+89126 -> 5*/
+
+Console.Clear();
+
+Console. WriteLine("Введите число ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+int GetLenghtNumberToStr(int value) 
+{
+    string spamNumber = value.ToString();
+    return spamNumber.Length;
+}
+int GetLenghtNumberMath(int value)
+{
+    if( value == 0)
+    {
+        return 1;
+    }
+    
+    int count = 0;
+    for(int i = 0; value >= 1; i++)
+    {
+        value /= 10;
+        count++;
+    }
+    return count;
+}
+Console. WriteLine(GetLenghtNumberMath(a));
