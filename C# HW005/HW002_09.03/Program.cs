e62437b04cc269917bd693ef8c3b6ccdb91257a4
Evgeniya <kapatoy@yandex.ru>
2023-03-16 16:17:26 +0300

    HW002_09.03

---
+++ C# HW005/HW002_09.03/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+Console.Clear();
+
+int [] FillArray(int size, int min, int max)
+{
+    int []result = new int [size];
+    for(int i = 0; i< result.Length; i++)
+    {
+        result[i] = new Random().Next(min, max);
+    }
+    return result;
+}
+int FindSumElements(int [] arr)
+{
+    int sum = 0;
+    for(int i = 1; i< arr.Length; i+=2)
+    {
+        sum = sum + arr[i];
+    }
+    return sum;
+}
+int [] array = FillArray(10, 1, 20);
+Console.WriteLine($"Сумма элементов, стоящих на нечетных позициях в массиве [{string.Join(", ", array)}], равна {FindSumElements(array)}");
