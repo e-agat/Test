3bd784ad6e0bf3b5e1fab6210fc2f6372c1ec90b
Evgeniya <kapatoy@yandex.ru>
2023-03-27 14:09:07 +0300

    Создаем метод для выявления второго массива

--- CW001/finalWork/Program.cs
+++ CW001/finalWork/Program.cs
@@ -8,5 +8,21 @@ Console.Clear();
 string[] array1 = new string[4] { "hello", "2", "world", ":-)" };
 string[] array2 = new string[array1.Length];
 
+void FindSecondArray(string[] array1, string[] array2)
+{
+
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[count] = array1[i];
+            count++;
+
+
+        }
+    }
+}
+
 
 
