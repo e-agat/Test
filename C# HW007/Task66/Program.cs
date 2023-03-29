caa298a0efa1ee65d0e55a00f10aeb32c6b72a5f
Evgeniya <kapatoy@yandex.ru>
2023-03-29 14:07:34 +0300

    Задачи 64, 66, 68

---
+++ C# HW007/Task66/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+Задача 66: Задайте значения M и N. 
+Напишите программу, которая найдёт сумму натуральных 
+элементов в промежутке от M до N с помощью рекурсии 
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+
+Console.WriteLine("Введите МЕНЬШЕЕ натуральное число");
+int numberM = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите БОЛЬШЕЕ натуральное число");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+
+
+Console.WriteLine($"M = {numberM}; N = {numberN} -> {SumOfNumbers(numberM, numberN)}");
+
+
+
+int SumOfNumbers (int numM, int numN)
+{
+    if(numM == numN + 1) return 0;
+    return numN + SumOfNumbers(numM, numN - 1);
+}
---
+++ C# HW007/Task68/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Задача 68: Задайте значения M и N. 
+Напишите программу, которая найдёт наибольший общий 
+делитель (НОД) этих чисел с помощью рекурсии.
+M = 28; N = 7 -> 7
+*/
+
+Console.WriteLine("Введите натуральное число M");
+int numberM = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите натуральное число N");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+
+
+Console.WriteLine($"M = {numberM}; N = {numberN} -> {Nod(numberM, numberN)}");
+
+int Nod(int n, int m)
+{
+    if (m == 0)
+        return n;
+    return Nod(m, n % m);
+}
